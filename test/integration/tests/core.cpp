5aac48eee84d03402af9b45bf4a8db7337c5f8f9
Michael Mestnik <cheako+github_com@mikemestnik.net>
2020-08-25 09:19:33 -0500

    Debug

--- test/integration/tests/core.cpp
+++ test/integration/tests/core.cpp
@@ -323,7 +323,7 @@ TEST_F(ZNCTest, UnixSocketUser) {
     out << "<User test>\nPass = test\nAdmin = true\n</User>\n";
     config.close();
 
-    system((const char*)("echo cat " + config.fileName()).data());
+    system((const char*)(QString("cat ") + config.fileName()).data());
 
     auto znc = Run();
 
